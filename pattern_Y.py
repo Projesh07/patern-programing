539e9b6d60be70fbab657e368d8f4a17894bff98
Projesh Bhoumik <projesh.bhoumik07@gmail.com>
2018-05-17 12:51:50 +0600

    Update pattern_Y.py

--- pattern_Y.py
+++ pattern_Y.py
@@ -1,14 +1,14 @@
 import time
 import sys
 
-# 	  0123456
+	# 	  0123456
 
-# 0	 *        *
-# 1	  *     *
-# 2     *  *
-# 3	     *   
-# 4	     *     
-# 5      *
+	# 0	 *      *
+	# 1	  *    *
+	# 2        *  *
+	# 3	    *   
+	# 4	    *     
+	# 5         *
 
 for row in range(7):
 	for col in range(7):
