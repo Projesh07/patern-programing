e40a5beb9eb5d5c81f75c38b9f01e345b901f8ba
Projesh Bhoumik <projesh.bhoumik07@gmail.com>
2019-09-27 15:44:00 +0200

    Update pattern_I.py

--- pattern_I.py
+++ pattern_I.py
@@ -20,3 +20,25 @@ for row in range(7):
 			print ' ',
 	print ''			
 	time.sleep(0.1)
+	
+a=[[1,2,3,4,5],
+[1,2,3,4,5],
+[1,2,3,4,5],
+[1,2,3,4,5],
+[1,2,3,4,5]
+]
+
+n=int(len(a)/2)
+
+
+for i in range(len(a)):
+    
+    for j in range(len(a[0])):
+
+        if (i==0 or i==len(a)-1) and j !=n:
+            print("*",end=" ")
+        elif(j==n):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print("\n")     
